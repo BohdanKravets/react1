52201dc107f347008273d9c10808ce705ee7409e
BohdanKravets <bohdan.kravets@hotmail.com>
2021-05-17 17:04:13 +0300

    random

--- src/App.js
+++ src/App.js
@@ -1,53 +1,7 @@
+export default function App() {
+    return(
+        <div>
 
-import './App.css';
-
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from 'react-router-dom';
-import Home from './components/home/Home';
-
-import Users from './components/users/Users';
-
-function App() {
-    return (
-        <Router>
-            <div>
-                <ul>
-                    <li>
-
-                        <Link to={'/'}>home</Link>
-                    </li>
-                    <li>
-                        <Link to={'/about'}>about</Link>
-
-                    </li>
-
-                    <li>
-                        <Link to={'/users'}>users</Link>
-
-                    </li>
-                    <hr/>
-
-                    <Switch>
-
-                        <Route exact path={'/'}>
-                            <Home/>
-                        </Route>
-
-
-                        <Route path={'/users'} component={Users}/>
-
-                    </Switch>
-                    <hr/>
-
-
-                </ul>
-            </div>
-        </Router>
+        </div>
     );
 }
-
-export default App;
