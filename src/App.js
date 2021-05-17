d900c5b6c87953e414f8d28d6945e9bb99451fc9
BohdanKravets <bohdan.kravets@hotmail.com>
2021-05-17 16:21:23 +0300

    practice 6

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Form from "./components/form/Form";
+
+export default function App() {
+    return(
+        <div>
+<Form/>
+        </div>
+    );
+}
---
+++ src/components/form/Form.js
@@ -0,0 +1,9 @@
+export default function Form() {
+    return(
+        <div>
+            <form >
+                <input type="number" name={'numberField'}/>
+            </form>
+        </div>
+    );
+}
