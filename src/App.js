c719d61780da4a63298148e24bc4472747a3e821
BohdanKravets <bohdan.kravets@hotmail.com>
2021-05-18 09:34:13 +0300

    hw5 part2

--- src/App.js
+++ src/App.js
@@ -9,7 +9,11 @@ import Home from "./components/home/Home";
 import Users from "./components/users/Users";
 import UserDetails from "./components/userDetails/UserDetails";
 
-
+// Беремо цю апі - https://reqres.in/
+// з неї використовувати тільки get запити на list users та single user.
+//     На головній сторінці є роут який одразу відображає  користувачів. (Зауважте, що апі не віддає всіх користувачів,а має queryParametr/serach з назвою page , який характеризує пагінацію, та віддає по 10 об'єктів за один запит).
+// Внизу є 2 кнопки при натисканні на які ви ідете до наступної/попередньої сторінки (змінюючи page параметр).
+// при клыку на користувача відбувається перехід на сторінку з детальною інформацією користувача
 export default function App() {
 
 
